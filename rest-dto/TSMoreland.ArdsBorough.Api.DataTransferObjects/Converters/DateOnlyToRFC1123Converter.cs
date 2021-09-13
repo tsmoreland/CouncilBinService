d803ce0c64ce714547158fc876ea876dc23137f5
Terry Moreland <tsmoreland@live.com>
2021-09-13 20:28:52 +0100

    DateOnly To RFC1123 converter - so we can serialize DateOnly in HTTP style date format

---
+++ rest-dto/TSMoreland.ArdsBorough.Api.DataTransferObjects/Converters/DateOnlyToRFC1123Converter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace TSMoreland.ArdsBorough.Api.DataTransferObjects.Converters;
+
+/// <summary>
+/// <see cref="DateOnly"/> to RFC1123 Converter
+/// </summary>
+public sealed class DateOnlyToRfc1123Converter : JsonConverter<DateOnly>
+{
+    /// <inheritdoc/>
+    public override DateOnly Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        if (reader.TokenType != JsonTokenType.String)
+        {
+            throw new JsonException("expected string");
+        }
+
+        //ddd, dd MMM yyyy HH':'mm':'ss 'GMT'
+        var value = reader.GetString() ?? string.Empty;
+
+        if (!DateTime.TryParseExact(value, "r", CultureInfo.InvariantCulture, DateTimeStyles.AssumeUniversal, out var dateValue))
+        {
+            throw new JsonException("invalid date format");
+        }
+
+        dateValue = dateValue.ToUniversalTime();
+
+        return new DateOnly(dateValue.Year, dateValue.Month, dateValue.Day);
+    }
+
+    /// <inheritdoc/>
+    public override void Write(Utf8JsonWriter writer, DateOnly value, JsonSerializerOptions options)
+    {
+        writer.WriteStringValue(value.ToDateTime(new TimeOnly()).ToString("r"));
+    }
+}
