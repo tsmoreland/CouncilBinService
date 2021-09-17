20ecbcc2f65bc68f2f8c497716d33b458a5c065b
Terry Moreland <tsmoreland@live.com>
2021-09-17 22:04:41 +0100

    added error controller, haven't used it yet

---
+++ rest-service/TSMoreland.ArdsBourgh.Api.App/Controllers/ErrorController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TSMoreland.ArdsBourgh.Api.App.Controllers;
+
+/// <summary>
+/// based on <a href="https://docs.microsoft.com/en-us/aspnet/core/web-api/handle-errors?view=aspnetcore-6.0">Handle errors in ASP.NET Core web APIs</a>
+/// </summary>
+[Route("api/[controller]")]
+[ApiVersion("1.0")]
+[ApiController]
+public class ErrorController : ControllerBase
+{
+    [Route("/error")]
+    public IActionResult Error()
+    {
+        return Problem();
+    }
+
+    [Route("/error-dev")]
+    public IActionResult ErrorLocalDevelopment([FromServices] IWebHostEnvironment webHostEnvironment)
+    {
+        if (webHostEnvironment.EnvironmentName != "Development")
+        {
+            throw new InvalidOperationException(
+                "This shouldn't be invoked in non-development environments.");
+        }
+
+        var context = HttpContext.Features.Get<IExceptionHandlerFeature>();
+        if (context is not null)
+        {
+            return Problem(
+                detail: context.Error.StackTrace,
+                title: context.Error.Message);
+        }
+        else
+        {
+            return Problem();
+        }
+    }
+}
