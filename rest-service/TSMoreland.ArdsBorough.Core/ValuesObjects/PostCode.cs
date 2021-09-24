d8d226d03088a4d61a60a8d346ef24f758a52897
Terry Moreland <tsmoreland@live.com>
2021-09-25 00:34:33 +0100

    update collection address to use postcode type updated postcode type with validation

--- rest-service/TSMoreland.ArdsBorough.Core/ValuesObjects/PostCode.cs
+++ rest-service/TSMoreland.ArdsBorough.Core/ValuesObjects/PostCode.cs
@@ -1,31 +1,37 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace TSMoreland.ArdsBorough.Core.ValuesObjects
 {
-    public record class PostCode
+    public readonly record struct PostCode : IEquatable<PostCode>
     {
+        private static readonly Regex _postcode = new (@"([Gg][Ii][Rr] 0[Aa]{2})|((([A-Za-z][0-9]{1,2})|(([A-Za-z][A-Ha-hJ-Yj-y][0-9]{1,2})|(([A-Za-z][0-9][A-Za-z])|([A-Za-z][A-Ha-hJ-Yj-y][0-9][A-Za-z]?))))\s?[0-9][A-Za-z]{2})");
 
         public PostCode(string value)
         {
+            if (!IsValid(value))
+            {
+                throw new ArgumentException("Invalid postcode", nameof(value));
+            }
+
             Value = value;
         }
-        public string Value { get; }
+
+        public string Value { get; init; } 
 
         public void Decontruct(out string value)
         {
             value = Value;
         }
 
-        public static bool IsValue(string postCode)
+        private static bool IsValid(string postCode)
         {
-            // use regex to check validity
-            return false;
+            return postCode is { Length: >0 } && _postcode.IsMatch(postCode);
         }
 
+        public override string ToString()
+        {
+            return Value;
+        }
     }
-
 }
