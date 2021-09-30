efa3cc55072296801f8aea0846dfbee725b23da6
Terry Moreland <tsmoreland@live.com>
2021-10-01 00:30:23 +0100

    initial service implementation providing bin collection details for given address (using current date)

---
+++ rest-service/TSMoreland.ArdsBorough.Api.WebServiceFacade.Service/UrpnRepository.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+namespace TSMoreland.ArdsBorough.Api.WebServiceFacade.Service
+{
+    public class UrpnRepository
+    {
+        private readonly IConfiguration _configuration;
+
+        public UrpnRepository(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public Task<(string Council, string URPN)> GetCouncilAndURPNFromAddressAsync(int houseNumber, string postcode, CancellationToken cancellationToken)
+        {
+            if (ValidateParametersOrThrow() is { } exception)
+            {
+                return Task.FromException<(string Council, string URPN)>(exception);
+            }
+
+            var council = _configuration["test-data:council"] ?? string.Empty;
+            var urpn = _configuration["test-data:urpn"] ?? string.Empty;
+
+            return Task.FromResult<(string Council, string URPN)>((council, urpn));
+
+            Exception? ValidateParametersOrThrow()
+            {
+                if (houseNumber <= 0)
+                {
+                    return new ArgumentOutOfRangeException(nameof(houseNumber));
+                }
+
+                if (postcode is not { Length: >0 })
+                {
+                    return new ArgumentException("invalid postcode", nameof(postcode));
+                }
+
+                return null;
+            }
+        }
+    }
+}
---
+++ rest-service/TSMoreland.ArdsBorough.Api.WebServiceFacade.Service/WebServiceFacadeService.cs
@@ -0,0 +1,45 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using ArdsBorough.WebService.External;
+using TSMoreland.ArdsBorough.Api.WebServiceFacade.Service;
+using TSMoreland.ArdsBorough.Api.WebServiceFacade.Shared;
+
+namespace TSMoreland.ArdsBorough.Api.WebServiceFacade.Service;
+
+public sealed class WebServiceFacadeService : IWebServiceFacade
+{
+    private readonly string _apiSecret;
+    private readonly UrpnRepository _urpnRepository;
+    private readonly WebService2Soap _soapApi;
+
+    public WebServiceFacadeService(string apiSecret, UrpnRepository urpnRepository, WebService2Soap soapApi)
+    {
+        _apiSecret = apiSecret;
+        _urpnRepository = urpnRepository ?? throw new ArgumentNullException(nameof(urpnRepository));
+        _soapApi = soapApi ?? throw new ArgumentNullException(nameof(soapApi));
+    }
+
+    /// <inheritdoc/>
+    public async IAsyncEnumerable<RoundInfo> GetRoundsForDate(string postcode, int houseNumber, DateOnly date, [EnumeratorCancellation] CancellationToken cancellationToken)
+    {
+        var (council, urpn) = await _urpnRepository.GetCouncilAndURPNFromAddressAsync(houseNumber, postcode, cancellationToken);
+
+        var xmlNode = await _soapApi.getRoundInfoForUPRNNewAsync(council, urpn, _apiSecret, date.ToString("dd/MM/yyyy"));
+        if (xmlNode == null)
+        {
+            // throw or at the very least log an error
+            yield break;
+        }
+
+        // sample output: Grey Bin: Mon 27 Sep then every alternate Mon 
+        // pass to RoundInfo domain object (different name?) and let that handle the parsing, even if it's just a static functional method,
+        // putting it in core highlights the primary test area
+
+
+
+        yield break;
+    }
+}
