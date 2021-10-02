2f6520309445045ffcc86293ebed7a27c1777b28
Terry Moreland <tsmoreland@live.com>
2021-10-02 22:53:32 +0100

    added infrastructure library for webservicefacade

---
+++ rest-service/TSMoreland.ArdsBorough.Api.WebServiceFacade.Infrastructure/WebService2SoapFactory.cs
@@ -0,0 +1,11 @@
+﻿using ArdsBorough.WebService.External;
+
+namespace TSMoreland.ArdsBorough.Api.WebServiceFacade.Infrastructure;
+
+public class WebService2SoapFactory
+{
+    public WebService2Soap Build()
+    {
+        return new WebService2SoapClient(WebService2SoapClient.EndpointConfiguration.WebService2Soap12);
+    }
+}
