388b3ca891440816b1dc82d9bba782a96b5bc761
Terry Moreland <tsmoreland@live.com>
2021-10-04 20:23:17 +0100

    add shared library for arsd borough

---
+++ rest-service/TSMoreland.ArdsBorough.Shared/AddressNotFoundException.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace TSMoreland.ArdsBorough.Shared;
+
+[Serializable]
+public sealed class AddressNotFoundException : Exception
+{
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="AddressNotFoundException"/> class
+    /// with address details
+    /// </summary>
+    /// <param name="houseNumber">House or Building number for the address</param>
+    /// <param name="postCode">Address Postcode</param>
+    public AddressNotFoundException(int houseNumber, PostCode postCode)
+        : this(houseNumber, postCode, null, null)
+    {
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="AddressNotFoundException"/> class
+    /// with address details, a specified error message 
+    /// </summary>
+    /// <param name="houseNumber">House or Building number for the address</param>
+    /// <param name="postCode">Address Postcode</param>
+    /// <param name="message">The error message that explains the reason for the exception.</param>
+    public AddressNotFoundException(int houseNumber, PostCode postCode, string? message)
+        : this(houseNumber, postCode, message, null)
+    {
+    }
+
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="AddressNotFoundException"/> class
+    /// with address details, a specified error message and a reference to the inner
+    /// exception that is the cause of this exception.
+    /// </summary>
+    /// <param name="houseNumber">House or Building number for the address</param>
+    /// <param name="postCode">Address Postcode</param>
+    /// <param name="message">The error message that explains the reason for the exception.</param>
+    /// <param name="innerException">
+    /// The exception that is the cause of the current exception, or a null reference
+    /// (Nothing in Visual Basic)
+    /// if no inner exception is specified.
+    /// </param>
+    public AddressNotFoundException(int houseNumber, PostCode postCode, string? message, Exception? innerException)
+        : base(message, innerException)
+    {
+        HouseNumber = houseNumber;
+        PostCode = postCode;
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the Exception class with serialized data.
+    /// </summary>
+    /// <param name="info">
+    /// The <see cref="SerializationInfo"/> that holds the serialized object
+    /// data about the exception being thrown
+    /// .</param>
+    /// <param name="context">
+    /// The <see cref="StreamingContext"/> that contains contextual information
+    /// about the source or destination.
+    /// </param>
+    private AddressNotFoundException(SerializationInfo info, StreamingContext context)
+        : base(info, context)
+    {
+        HouseNumber = info.GetInt32(nameof(HouseNumber));
+        var postCode = info.GetString(nameof(PostCode)) ?? string.Empty;
+        PostCode = new PostCode(postCode);
+    }
+
+    /// <inheritdoc/>
+    public override void GetObjectData(SerializationInfo info, StreamingContext context)
+    {
+        base.GetObjectData(info, context);
+
+        info.AddValue(nameof(HouseNumber), HouseNumber);
+        info.AddValue(nameof(PostCode), PostCode.Value);
+    }
+
+    /// <summary>
+    /// House or Building number for the address
+    /// </summary>
+    public int HouseNumber { get; }
+
+    /// <summary>
+    /// Address Postcode
+    /// </summary>
+    public PostCode PostCode { get; }
+}
---
+++ rest-service/TSMoreland.ArdsBorough.Shared/PostCode.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace TSMoreland.ArdsBorough.Shared;
+
+public readonly record struct PostCode : IEquatable<PostCode>
+{
+    private static readonly Regex _postcode = new (@"([Gg][Ii][Rr] 0[Aa]{2})|((([A-Za-z][0-9]{1,2})|(([A-Za-z][A-Ha-hJ-Yj-y][0-9]{1,2})|(([A-Za-z][0-9][A-Za-z])|([A-Za-z][A-Ha-hJ-Yj-y][0-9][A-Za-z]?))))\s?[0-9][A-Za-z]{2})");
+
+    public PostCode(string value)
+    {
+        if (!IsValid(value))
+        {
+            throw new ArgumentException("Invalid postcode", nameof(value));
+        }
+
+        Value = value;
+    }
+
+    public string Value { get; init; } 
+
+    public void Decontruct(out string value)
+    {
+        value = Value;
+    }
+
+    private static bool IsValid(string postCode)
+    {
+        return postCode is { Length: >0 } && _postcode.IsMatch(postCode);
+    }
+
+    public override string ToString()
+    {
+        return Value;
+    }
+}
