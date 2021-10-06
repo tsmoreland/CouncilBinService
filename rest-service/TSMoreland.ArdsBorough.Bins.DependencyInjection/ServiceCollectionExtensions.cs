e2cf935dbc4057b4fd3c9766228ab0ca549bd468
Terry Moreland <tsmoreland@live.com>
2021-10-07 00:30:59 +0100

    dependency injection for bins service

---
+++ rest-service/TSMoreland.ArdsBorough.Bins.DependencyInjection/ServiceCollectionExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using TSMoreland.ArdsBorough.Bins.Shared;
+using TSMoreland.ArdsBorough.Bins.Services;
+
+namespace TSMoreland.ArdsBorough.Bins.DependencyInjection;
+
+public static class ServiceCollectionExtensions
+{
+
+    public static IServiceCollection AddBinsCollection(this IServiceCollection services)
+    {
+        if (services == null!)
+        {
+            throw new ArgumentNullException(nameof(services));
+        }
+
+        services.AddScoped<IBinCollectionService, BinCollectionService>();
+
+        return services;
+    }
+}
