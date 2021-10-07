e00c262741d27679f25545ad0dcf6ffce5efadca
Terry Moreland <tsmoreland@live.com>
2021-10-07 01:18:14 +0100

    infrastructure library - handles DI and AutoMapper setup (for converting from shared internal dto to API dto)

---
+++ rest-service/TSMoreland.ArdsBorough.Api.Infrastructure/Profiles/MappingProfile.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using AutoMapper;
+using AutoMapper.Extensions.EnumMapping;
+using BinsDomain = TSMoreland.ArdsBorough.Bins.Shared;
+using DTO = TSMoreland.ArdsBorough.Api.DataTransferObjects;
+
+namespace TSMoreland.ArdsBorough.Api.Infrastructure.Profiles;
+
+public sealed class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        CreateMap<BinsDomain.BinType, DTO.BinType>()
+            .ConvertUsingEnumMapping<BinsDomain.BinType, DTO.BinType>(options =>  options.MapByName(false))
+            .ReverseMap();
+
+        CreateMap<(BinsDomain.BinType Type, DateOnly Date), DTO.Response.BinCollectionSummary>()
+            .ConstructUsing((source, context) => 
+                new DTO.Response.BinCollectionSummary(
+                    context.Mapper.Map<DTO.BinType>(source.Type), 
+                    source.Date.ToDateTime(new TimeOnly(7, 0, 0))));
+    }
+}
---
+++ rest-service/TSMoreland.ArdsBorough.Api.Infrastructure/ServiceCollectionExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using TSMoreland.ArdsBorough.Api.WebServiceFacade.DependencyInjection;
+using TSMoreland.ArdsBorough.Bins.DependencyInjection;
+
+namespace TSMoreland.ArdsBorough.Api.Infrastructure;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddApiInfrastructure(this IServiceCollection services)
+    {
+        if (services == null!)
+        {
+            throw new ArgumentNullException(nameof(services));
+        }
+
+        services.AddAutoMapper(typeof(Profiles.MappingProfile).Assembly);
+
+        services.AddWebServiceFacade();
+        services.AddBinsCollection();
+
+        return services;
+    }
+}
