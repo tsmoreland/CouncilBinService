fccc8d854c472946159b905b5c5aea5bd8f3376f
Terry Moreland <tsmoreland@live.com>
2021-10-08 01:10:50 +0100

    added method for querying the current and next week

--- rest-service/TSMoreland.ArdsBorough.Bins.Services/BinCollectionService.cs
+++ rest-service/TSMoreland.ArdsBorough.Bins.Services/BinCollectionService.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Sockets;
+using System.Reflection.Metadata.Ecma335;
 using System.Threading;
 using Microsoft.Extensions.Configuration;
 using TSMoreland.ArdsBorough.Bins.Core.Models;
@@ -24,12 +25,50 @@ public sealed class BinCollectionService : IBinCollectionService
     }
 
     /// <inheritdoc/>
-    public IAsyncEnumerable<(BinType Type, DateOnly Date)> FindNextBinCollectionInfoForAddress(int houseNumber, PostCode postCode, CancellationToken cancellationToken)
+    public IAsyncEnumerable<(BinType Type, DateOnly Date)> FindBinCollectionInfoForAddress(int houseNumber, PostCode postCode, CancellationToken cancellationToken)
     {
         return _webServiceFacade
             .GetRoundsForDate(postCode.Value, houseNumber, DateOnly.FromDateTime(DateTime.Now), cancellationToken)
             .Select(RoundInfo.ParseOrNone)
             .Where(info => info != RoundInfo.None)
+            .OrderBy(info => info.Collection)
+            .Select(info => (info.Type, info.Collection))
+            .AsAsyncEnumerable();
+    }
+
+    public IAsyncEnumerable<(BinType Type, DateOnly Date)> FindThisWeeksBinCollectionInfoForAddress(int houseNumber, PostCode postCode,
+        CancellationToken cancellationToken)
+    {
+        var now = DateOnly.FromDateTime(DateTime.UtcNow);
+        int dayNumber = (int) now.DayOfWeek;
+
+        var weekStart = now.AddDays((int) DayOfWeek.Sunday - dayNumber);
+        var weekEnd = now.AddDays((int) DayOfWeek.Saturday - dayNumber);
+
+        return FindBinCollectionInfoForAddressWithDateRange(houseNumber, postCode, weekStart, weekEnd, cancellationToken);
+    }
+
+    public IAsyncEnumerable<(BinType Type, DateOnly Date)> FindNextWeeksBinCollectionInfoForAddress(int houseNumber, PostCode postCode, CancellationToken cancellationToken)
+    {
+        var now = DateOnly.FromDateTime(DateTime.UtcNow);
+        int dayNumber = (int) now.DayOfWeek;
+
+        var weekStart = now.AddDays((int) DayOfWeek.Sunday - dayNumber + 7);
+        var weekEnd = now.AddDays((int) DayOfWeek.Saturday - dayNumber + 7);
+
+        return FindBinCollectionInfoForAddressWithDateRange(houseNumber, postCode, weekStart, weekEnd, cancellationToken);
+    }
+
+    private IAsyncEnumerable<(BinType Type, DateOnly Date)> FindBinCollectionInfoForAddressWithDateRange(int houseNumber, PostCode postCode, DateOnly weekStart, DateOnly weekEnd,
+        CancellationToken cancellationToken)
+    {
+        var now = DateOnly.FromDateTime(DateTime.UtcNow);
+
+        return _webServiceFacade
+            .GetRoundsForDate(postCode.Value, houseNumber, DateOnly.FromDateTime(DateTime.Now), cancellationToken)
+            .Select(RoundInfo.ParseOrNone)
+            .Where(info => info != RoundInfo.None)
+            .Where(info => info.Collection >= weekStart && info.Collection <= weekEnd)
             .OrderBy(info => info.Collection)
             .Select(info => (info.Type, info.Collection))
             .AsAsyncEnumerable();
--- rest-service/TSMoreland.ArdsBorough.Bins.Shared/IBinCollectionService.cs
+++ rest-service/TSMoreland.ArdsBorough.Bins.Shared/IBinCollectionService.cs
@@ -7,7 +7,7 @@ namespace TSMoreland.ArdsBorough.Bins.Shared;
 public interface IBinCollectionService
 {
     /// <summary>
-    /// Returns the next bins collected and day they're collected on 
+    /// Returns the all bins collection types and date for given address
     /// </summary>
     /// <param name="houseNumber">house or building number used to look up address</param>
     /// <param name="postCode">postcode used to look up address</param>
@@ -18,5 +18,33 @@ public interface IBinCollectionService
     /// <exception cref="AddressNotFoundException">
     /// if address is invalid or cannot be found
     /// </exception>
-    IAsyncEnumerable<(BinType Type, DateOnly Date)> FindNextBinCollectionInfoForAddress(int houseNumber, PostCode postCode, CancellationToken cancellationToken);
+    IAsyncEnumerable<(BinType Type, DateOnly Date)> FindBinCollectionInfoForAddress(int houseNumber, PostCode postCode, CancellationToken cancellationToken);
+
+    /// <summary>
+    /// Returns the bins collection types and date for given address for the current week
+    /// </summary>
+    /// <param name="houseNumber">house or building number used to look up address</param>
+    /// <param name="postCode">postcode used to look up address</param>
+    /// <param name="cancellationToken">A cancellation token.</param>
+    /// <returns>
+    /// asynchronous collection of bins collection info for the next collection
+    /// </returns>
+    /// <exception cref="AddressNotFoundException">
+    /// if address is invalid or cannot be found
+    /// </exception>
+    IAsyncEnumerable<(BinType Type, DateOnly Date)> FindThisWeeksBinCollectionInfoForAddress(int houseNumber, PostCode postCode, CancellationToken cancellationToken);
+
+    /// <summary>
+    /// Returns the bins collection types and date for given address for the next week
+    /// </summary>
+    /// <param name="houseNumber">house or building number used to look up address</param>
+    /// <param name="postCode">postcode used to look up address</param>
+    /// <param name="cancellationToken">A cancellation token.</param>
+    /// <returns>
+    /// asynchronous collection of bins collection info for the next collection
+    /// </returns>
+    /// <exception cref="AddressNotFoundException">
+    /// if address is invalid or cannot be found
+    /// </exception>
+    IAsyncEnumerable<(BinType Type, DateOnly Date)> FindNextWeeksBinCollectionInfoForAddress(int houseNumber, PostCode postCode, CancellationToken cancellationToken);
 }
