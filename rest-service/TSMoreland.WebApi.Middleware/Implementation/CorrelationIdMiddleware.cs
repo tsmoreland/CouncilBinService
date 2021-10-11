936856fcf692ba4bff8e6342cdbb0f6bb4f0d503
Terry Moreland <tsmoreland@live.com>
2021-10-11 20:07:54 +0100

    moved middlewware implementation to subdirectory

--- rest-service/TSMoreland.WebApi.Middleware/Implementation/CorrelationIdMiddleware.cs
+++ rest-service/TSMoreland.WebApi.Middleware/Implementation/CorrelationIdMiddleware.cs
@@ -2,7 +2,7 @@
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
-namespace TSMoreland.WebApi.Middleware;
+namespace TSMoreland.WebApi.Middleware.Implementation;
 
 public sealed  class CorrelationIdMiddleware
 {
