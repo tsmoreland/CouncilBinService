376c1cd3a3963afa22311f761ee6fbd6ac781017
Terry Moreland <tsmoreland@live.com>
2021-10-13 21:16:44 +0100

    initial xml comments that will show up in swagger doc

--- rest-service/TSMoreland.ArdsBorourgh.Api.App/Controllers/BinsController.cs
+++ rest-service/TSMoreland.ArdsBorourgh.Api.App/Controllers/BinsController.cs
@@ -1,17 +1,21 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Net.Mime;
 using System.Threading;
 using System.Threading.Tasks;
 using AutoMapper;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Swashbuckle.AspNetCore.Annotations;
 using TSMoreland.ArdsBorough.Api.DataTransferObjects.Response;
 using BinsDomain = TSMoreland.ArdsBorough.Bins.Shared;
 using DTO = TSMoreland.ArdsBorough.Api.DataTransferObjects;
 
 namespace TSMoreland.ArdsBorough.Api.App.Controllers;
 
+/// <summary/>
 [Route("api/v{version:apiVersion}/bins")]
 [ApiVersion("1")]
 [ApiController]
@@ -21,6 +25,7 @@ public class BinsController : ControllerBase
     private readonly IMapper _mapper;
     private readonly ILogger<BinsController> _logger;
 
+    /// <summary/>
     public BinsController(BinsDomain.IBinCollectionService binCollectionService, IMapper mapper, ILogger<BinsController> logger)
     {
         _binCollectionService = binCollectionService ?? throw new ArgumentNullException(nameof(binCollectionService));
@@ -28,10 +33,20 @@ public class BinsController : ControllerBase
         _logger = logger ?? throw new System.ArgumentNullException(nameof(logger));
     }
 
+    /// <summary>
+    /// Get a summary of all upcoming bin collection details
+    /// </summary>
+    /// <param name="postcode" example="SW1A 1AA" >postcode of the address to find details of</param>
+    /// <param name="houseNumber" example="1" >house number of the address to find details of</param>
+    /// <param name="cancellationToken">A cancellation token.</param>
+    /// <returns>a collection of <see cref="BinCollectionSummary"/></returns>
     [HttpGet]
     [Route("{postcode}/{houseNumber:int}")]
     [Consumes(MediaTypeNames.Application.Json)]
     [Produces(MediaTypeNames.Application.Json)]
+    [SwaggerResponse(StatusCodes.Status200OK, "Successful response.", typeof(List<BinCollectionSummary>), MediaTypeNames.Application.Json)] 
+    [SwaggerResponse(StatusCodes.Status404NotFound, "Address not found.", typeof(ProblemDetails), MediaTypeNames.Application.Json)] 
+    [SwaggerResponse(StatusCodes.Status400BadRequest, "Invalid argument.", typeof(ProblemDetails), MediaTypeNames.Application.Json)] 
     public async Task<IActionResult> GetAllUpcoming([FromRoute] string postcode, [FromRoute] int houseNumber, CancellationToken cancellationToken)
     {
         var collections = await _binCollectionService
@@ -42,11 +57,20 @@ public class BinsController : ControllerBase
         return Ok(collections);
     }
 
-
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="postcode"></param>
+    /// <param name="houseNumber"></param>
+    /// <param name="cancellationToken">A cancellation token.</param>
+    /// <returns>a collection of <see cref="BinCollectionSummary"/></returns>
     [HttpGet]
     [Route("{postcode}/{houseNumber:int}/current")]
     [Consumes(MediaTypeNames.Application.Json)]
     [Produces(MediaTypeNames.Application.Json)]
+    [SwaggerResponse(StatusCodes.Status200OK, "Successful response.", typeof(List<BinCollectionSummary>), MediaTypeNames.Application.Json)] 
+    [SwaggerResponse(StatusCodes.Status404NotFound, "Address not found.", typeof(ProblemDetails), MediaTypeNames.Application.Json)] 
+    [SwaggerResponse(StatusCodes.Status400BadRequest, "Invalid argument.", typeof(ProblemDetails), MediaTypeNames.Application.Json)] 
     public async Task<IActionResult> GetThisWeeksType([FromRoute] string postcode, [FromRoute] int houseNumber, CancellationToken cancellationToken)
     {
         var collection = await _binCollectionService
@@ -57,10 +81,20 @@ public class BinsController : ControllerBase
         return Ok(collection);
     }
 
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="postcode"></param>
+    /// <param name="houseNumber"></param>
+    /// <param name="cancellationToken">A cancellation token.</param>
+    /// <returns>a collection of <see cref="BinCollectionSummary"/></returns>
     [HttpGet]
     [Route("{postcode}/{houseNumber:int}/next")]
     [Consumes(MediaTypeNames.Application.Json)]
     [Produces(MediaTypeNames.Application.Json)]
+    [SwaggerResponse(StatusCodes.Status200OK, "Successful response.", typeof(List<BinCollectionSummary>), MediaTypeNames.Application.Json)] 
+    [SwaggerResponse(StatusCodes.Status404NotFound, "Address not found.", typeof(ProblemDetails), MediaTypeNames.Application.Json)] 
+    [SwaggerResponse(StatusCodes.Status400BadRequest, "Invalid argument.", typeof(ProblemDetails), MediaTypeNames.Application.Json)] 
     public async Task<IActionResult> GetNextWeeksType([FromRoute] string postcode, [FromRoute] int houseNumber, CancellationToken cancellationToken)
     {
         var collection = await _binCollectionService
@@ -71,30 +105,60 @@ public class BinsController : ControllerBase
         return Ok(collection);
     }
 
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="postcode"></param>
+    /// <param name="houseNumber"></param>
+    /// <param name="cancellationToken">A cancellation token.</param>
+    /// <returns>a collection of <see cref="BinCollectionSummary"/></returns>
     [HttpGet]
     [Route("{postcode}/{houseNumber}/details")]
     [Consumes(MediaTypeNames.Application.Json)]
     [Produces(MediaTypeNames.Application.Json)]
-    public IActionResult GetFullDetailsForThisWeek([FromRoute] string postcode, [FromRoute] int houseNumber)
+    [SwaggerResponse(StatusCodes.Status200OK, "Successful response.", typeof(List<BinCollectionSummary>), MediaTypeNames.Application.Json)] 
+    [SwaggerResponse(StatusCodes.Status404NotFound, "Address not found.", typeof(ProblemDetails), MediaTypeNames.Application.Json)] 
+    [SwaggerResponse(StatusCodes.Status400BadRequest, "Invalid argument.", typeof(ProblemDetails), MediaTypeNames.Application.Json)] 
+    public IActionResult GetFullDetailsForThisWeek([FromRoute] string postcode, [FromRoute] int houseNumber, CancellationToken cancellationToken)
     {
         return Ok("blue");
     }
 
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="postcode"></param>
+    /// <param name="houseNumber"></param>
+    /// <param name="binType"></param>
+    /// <param name="cancellationToken">A cancellation token.</param>
+    /// <returns>a collection of <see cref="BinCollectionSummary"/></returns>
     [HttpGet]
     [Route("{postcode}/{houseNumber}/{binType}")]
     [Consumes(MediaTypeNames.Application.Json)]
     [Produces(MediaTypeNames.Application.Json)]
-    public IActionResult GetNextDateForType([FromRoute] string postcode, [FromRoute] int houseNumber, [FromRoute] DTO.BinType binType)
+    [SwaggerResponse(StatusCodes.Status404NotFound, "Address not found.", typeof(ProblemDetails), MediaTypeNames.Application.Json)] 
+    [SwaggerResponse(StatusCodes.Status400BadRequest, "Invalid argument.", typeof(ProblemDetails), MediaTypeNames.Application.Json)] 
+    public IActionResult GetNextDateForType([FromRoute] string postcode, [FromRoute] int houseNumber, [FromRoute] DTO.BinType binType, CancellationToken cancellationToken)
     {
         return Ok("blue");
     }
 
 
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="postcode"></param>
+    /// <param name="houseNumber"></param>
+    /// <param name="binType"></param>
+    /// <param name="cancellationToken">A cancellation token.</param>
+    /// <returns>a collection of <see cref="BinCollectionSummary"/></returns>
     [HttpGet]
     [Route("{postcode}/{houseNumber}/{binType}/period")]
     [Consumes(MediaTypeNames.Application.Json)]
     [Produces(MediaTypeNames.Application.Json)]
-    public IActionResult GetCollectionPeriod([FromRoute] string postcode, [FromRoute] int houseNumber, [FromRoute] DTO.BinType binType)
+    [SwaggerResponse(StatusCodes.Status404NotFound, "Address not found.", typeof(ProblemDetails), MediaTypeNames.Application.Json)] 
+    [SwaggerResponse(StatusCodes.Status400BadRequest, "Invalid argument.", typeof(ProblemDetails), MediaTypeNames.Application.Json)] 
+    public IActionResult GetCollectionPeriod([FromRoute] string postcode, [FromRoute] int houseNumber, [FromRoute] DTO.BinType binType, CancellationToken cancellationToken)
     {
         return Ok("blue");
     }
