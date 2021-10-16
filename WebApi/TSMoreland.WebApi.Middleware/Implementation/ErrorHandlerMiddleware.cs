e2b56f4909f968efbc4a72ef1d3cfb83c75df5f3
Terry Moreland <tsmoreland@live.com>
2021-10-16 01:26:18 +0100

    move middleware library

---
+++ WebApi/TSMoreland.WebApi.Middleware/Implementation/ErrorHandlerMiddleware.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace TSMoreland.WebApi.Middleware.Implementation
+{
+    public sealed class ErrorHandlerMiddleware
+    {
+        private readonly ExceptionHandlerMiddleware _middleware;
+
+        public ErrorHandlerMiddleware(RequestDelegate next, ILoggerFactory loggerFactory, IOptions<ExceptionHandlerOptions> options, DiagnosticListener diagnosticListener)
+        {
+            _middleware = new ExceptionHandlerMiddleware(next, loggerFactory, options, diagnosticListener);
+        }
+
+        public Task Invoke(HttpContext context)
+        {
+            return _middleware.Invoke(context);
+        }
+    }
+}
