ab47b4406916814898fe94804e7d57d04c1a87ee
Terry Moreland <tsmoreland@live.com>
2021-10-16 01:25:39 +0100

    move infrastructure library

--- Infrastructure/TSMoreland.ArdsBorough.Infrastructure/Profiles/MappingProfile.cs
+++ Infrastructure/TSMoreland.ArdsBorough.Infrastructure/Profiles/MappingProfile.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using AutoMapper;
 using AutoMapper.Extensions.EnumMapping;
-using BinsDomain = TSMoreland.ArdsBorough.Bins.Shared;
+using TSMoreland.ArdsBorough.Bins.Collections.Shared;
 using DTO = TSMoreland.ArdsBorough.Api.DataTransferObjects;
 
 namespace TSMoreland.ArdsBorough.Api.Infrastructure.Profiles;
@@ -10,11 +10,11 @@ public sealed class MappingProfile : Profile
 {
     public MappingProfile()
     {
-        CreateMap<BinsDomain.BinType, DTO.BinType>()
-            .ConvertUsingEnumMapping<BinsDomain.BinType, DTO.BinType>(options =>  options.MapByName(false))
+        CreateMap<BinType, DTO.BinType>()
+            .ConvertUsingEnumMapping<BinType, DTO.BinType>(options =>  options.MapByName(false))
             .ReverseMap();
 
-        CreateMap<(BinsDomain.BinType Type, DateOnly Date), DTO.Response.BinCollectionSummary>()
+        CreateMap<(BinType Type, DateOnly Date), DTO.Response.BinCollectionSummary>()
             .ConstructUsing((source, context) => 
                 new DTO.Response.BinCollectionSummary(
                     context.Mapper.Map<DTO.BinType>(source.Type), 
