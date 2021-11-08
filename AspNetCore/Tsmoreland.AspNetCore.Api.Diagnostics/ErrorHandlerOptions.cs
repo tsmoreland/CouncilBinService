5a33e6243905d2f9278487851a1941e0203cd555
Terry Moreland <tsmoreland@live.com>
2021-11-08 20:06:58 +0000

    error handler options

---
+++ AspNetCore/Tsmoreland.AspNetCore.Api.Diagnostics/ErrorHandlerOptions.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Tsmoreland.AspNetCore.Api.Diagnostics;
+
+public sealed class ErrorHandlerOptions
+{
+    public string? TraceIdentifier { get; set; } = null;
+
+    public List<string> HeadersToKeep { get; } = new();
+}
