ccd234e72021cfde94c6c06bd8790ff8a1403ce6
Terry Moreland <tsmoreland@live.com>
2021-11-13 21:56:49 +0000

    extension methods to get response content-type for errors based on accept header

---
+++ AspNetCore/Tsmoreland.AspNetCore.Api.Diagnostics/HttpContextExtensions.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Net.Mime;
+using Microsoft.AspNetCore.Http;
+
+namespace Tsmoreland.AspNetCore.Api.Diagnostics;
+
+public static class HttpContextExtensions
+{
+    public static string GetProblemResponseTypeFromAccept(this HttpContext context)
+    {
+        var acceptTypes = context.Request.Headers.Accept;
+
+        bool json = false;
+        bool xml = false;
+
+        foreach (var acceptType in acceptTypes)
+        {
+            if (!json && string.Equals(acceptType, MediaTypeNames.Application.Json, StringComparison.OrdinalIgnoreCase))
+            {
+                json = true;
+            }
+            if (!json && string.Equals(acceptType, MediaTypeNames.Application.Xml, StringComparison.OrdinalIgnoreCase))
+            {
+                xml = true;
+            }
+
+            if (json && xml)
+            {
+                break;
+            }
+        }
+
+        if (xml && !json)
+        {
+            return "application/problem+xml";
+        }
+        else
+        {
+            return "application/problem+json";
+        }
+    }
+}
