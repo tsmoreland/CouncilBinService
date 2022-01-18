7237ad3f3007f7b1be9f5ea782053ab2bae48dc2
Terry Moreland <tsmoreland@live.com>
2022-01-18 00:31:03 +0000

    dto formatting updates

--- DataTransferObjects/TSMoreland.ArdsBorough.Api.DataTransferObjects/SerializerOptions.cs
+++ DataTransferObjects/TSMoreland.ArdsBorough.Api.DataTransferObjects/SerializerOptions.cs
@@ -1,5 +1,5 @@
 ﻿//
-// Copyright © 2021 Terry Moreland
+// Copyright © 2022 Terry Moreland
 // Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), 
 // to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
 // and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
@@ -9,10 +9,8 @@
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
 // WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-// 
+//
 
-using System;
-using System.Linq;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 using TSMoreland.ArdsBorough.Api.DataTransferObjects.Converters;
@@ -24,11 +22,11 @@ namespace TSMoreland.ArdsBorough.Api.DataTransferObjects;
 /// </summary>
 public static class SerializerOptions
 {
-    private static readonly Lazy<JsonSerializerOptions> _options = new(BuildJsonSerializerOptions);
+    private static readonly Lazy<JsonSerializerOptions> s_options = new(BuildJsonSerializerOptions);
     /// <summary>
     /// Options used by the server 
     /// </summary>
-    public static JsonSerializerOptions Options => _options.Value;
+    public static JsonSerializerOptions Options => s_options.Value;
 
     /// <summary>
     /// Constructs default serializer settings, used to construct <see cref="Options"/>
