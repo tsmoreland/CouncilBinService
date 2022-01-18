3aafcab7e53a08cd95e0d99cc80abe4c6460738c
Terry Moreland <tsmoreland@live.com>
2022-01-18 00:27:53 +0000

    formatting + namespace corrections

--- Bins.Collections/TSMoreland.ArdsBorough.Bins.Collections.Services/BinCollectionService.cs
+++ Bins.Collections/TSMoreland.ArdsBorough.Bins.Collections.Services/BinCollectionService.cs
@@ -1,15 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Sockets;
-using System.Reflection.Metadata.Ecma335;
-using System.Threading;
+﻿//
+// Copyright © 2022 Terry Moreland
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), 
+// to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+// and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
 using Microsoft.Extensions.Configuration;
-using TSMoreland.ArdsBorough.Bins.Core.Models;
 using TSMoreland.ArdsBorough.Api.WebServiceFacade.Shared;
+using TSMoreland.ArdsBorough.Bins.Collections.Core.Models;
 using TSMoreland.ArdsBorough.Bins.Collections.Shared;
 
-namespace TSMoreland.ArdsBorough.Bins.Services;
+namespace TSMoreland.ArdsBorough.Bins.Collections.Services;
 
 public sealed class BinCollectionService : IBinCollectionService
 {
@@ -40,10 +47,10 @@ public sealed class BinCollectionService : IBinCollectionService
         CancellationToken cancellationToken)
     {
         var now = DateOnly.FromDateTime(DateTime.UtcNow);
-        int dayNumber = (int) now.DayOfWeek;
+        int dayNumber = (int)now.DayOfWeek;
 
-        var weekStart = now.AddDays((int) DayOfWeek.Sunday - dayNumber);
-        var weekEnd = now.AddDays((int) DayOfWeek.Saturday - dayNumber);
+        var weekStart = now.AddDays((int)DayOfWeek.Sunday - dayNumber);
+        var weekEnd = now.AddDays((int)DayOfWeek.Saturday - dayNumber);
 
         return FindBinCollectionInfoForAddressWithDateRange(houseNumber, postCode, weekStart, weekEnd, cancellationToken);
     }
@@ -51,10 +58,10 @@ public sealed class BinCollectionService : IBinCollectionService
     public IAsyncEnumerable<(BinType Type, DateOnly Date)> FindNextWeeksBinCollectionInfoForAddress(int houseNumber, PostCode postCode, CancellationToken cancellationToken)
     {
         var now = DateOnly.FromDateTime(DateTime.UtcNow);
-        int dayNumber = (int) now.DayOfWeek;
+        int dayNumber = (int)now.DayOfWeek;
 
-        var weekStart = now.AddDays((int) DayOfWeek.Sunday - dayNumber + 7);
-        var weekEnd = now.AddDays((int) DayOfWeek.Saturday - dayNumber + 7);
+        var weekStart = now.AddDays((int)DayOfWeek.Sunday - dayNumber + 7);
+        var weekEnd = now.AddDays((int)DayOfWeek.Saturday - dayNumber + 7);
 
         return FindBinCollectionInfoForAddressWithDateRange(houseNumber, postCode, weekStart, weekEnd, cancellationToken);
     }
