2b2c335edea3399659e9e37c175baa918371c5c0
Terry Moreland <tsmoreland@live.com>
2022-01-20 12:34:19 +0000

    copyright + formatting

--- WebApi/TSMoreland.WebApi.Middleware/Implementation/CorrelationIdMiddleware.cs
+++ WebApi/TSMoreland.WebApi.Middleware/Implementation/CorrelationIdMiddleware.cs
@@ -1,10 +1,23 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿//
+// Copyright © 2022 Terry Moreland
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), 
+// to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+// and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
 namespace TSMoreland.WebApi.Middleware.Implementation;
 
-public sealed  class CorrelationIdMiddleware
+public sealed class CorrelationIdMiddleware
 {
     private readonly RequestDelegate _next;
     private readonly IOptionsMonitor<CorrelationIdOptions> _options;
@@ -20,13 +33,13 @@ public sealed  class CorrelationIdMiddleware
     public async Task Invoke(HttpContext context)
     {
         _logger.LogInformation("Begin Request {CorrectionId}", context.TraceIdentifier);
-        var header = _options.CurrentValue.HeaderName;
+        string header = _options.CurrentValue.HeaderName;
         if (header is not { Length: > 0 })
         {
             header = CorrelationIdOptions.DefaultHeaderName;
         }
 
-        context.Response.Headers.Add("X-Correction-Id", context.TraceIdentifier);
+        context.Response.Headers.Add(header, context.TraceIdentifier);
         await _next(context);
         _logger.LogInformation("End Request {CorrectionId}", context.TraceIdentifier);
     }
